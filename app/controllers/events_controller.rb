53444d5417d7731d9cac9a406aab72d2adbd80a0
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-09-29 03:40:56 -0400

    added events controller and functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,27 @@
 class EventsController < ApplicationController
+
   def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      flash[:success] = "Event created"
+      redirect_to root_url
+  end
+end
+
+private
+  def event_params
+    params.require(:event).permit(:name, :description, :place, :time)
   end
 end
